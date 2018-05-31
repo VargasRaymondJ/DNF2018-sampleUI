dee0455fb3ff763916032249aaf295d823f462a7
Raymond Vargas <vargas_raymond@bah.com>
2018-05-30 21:06:01 -0400

    ag-grid integrated

--- src/components/Dogs.vue
+++ src/components/Dogs.vue
@@ -5,12 +5,7 @@
           <div class="col-lg-8 mx-auto">
             <h2>Available dogs</h2>
             <hr>
-            <ag-grid-vue style="width: 750px; height: 250px;"
-                class="ag-theme-material"
-                :gridOptions="gridOptions"
-                :columnDefs="columnDefs"
-                :rowData="rowData">
-            </ag-grid-vue>
+            <app-grid :rows="mammals"></app-grid>
           </div>
         </div>
       </div>
@@ -18,55 +13,27 @@
 </template>
 
 <script>
-import {AgGridVue} from "ag-grid-vue";
+import Grid from './grid/Grid';
 export default {
     data()  {
         return {
-            dogs: [
-                {name: "Puggle", species: "Puggle", age: 5, color: "black", sex: "female"},
-                {name: "Jack", species: "Lab", age: 1, color: "black", sex: "male"},
-                {name: "Koda", species: "Husky", age: 8, color: "Red and White", sex: "male"},
-                {name: "Bruce", species: "Mastiff", age: 3, color: "Brown", sex: "male"},
-                {name: "Lola", species: "French Bulldog", age: 2, color: "Grey", sex: "female"}
-            ],
-            gridOptions: {},
-            columnDefs: null,
-            rowData: null
-        }
-    },
-    methods: {
-        foo() {
-            this.$http.get('https://vuejs-http-a4289.firebaseio.com/data.json')
-            .then(res => {
-                console.log(res);
-            }
-            )
+            mammals: [],
         }
     },
     components: {
-        'ag-grid-vue': AgGridVue
-        },
-        created: function(){
-            this.foo();
-        },
-        beforeMount() {
-            this.columnDefs = [
-                {headerName: 'Name', field: 'name'},
-                {headerName: 'Species', field: 'species'},
-                {headerName: 'Age', field: 'age'},
-                {headerName: 'Color', field: 'color'},
-                {headerName: 'Sex', field: 'sex'},
-            ];
-
-            this.rowData = [
-                {name: "Puggle", species: "Puggle", age: 5, color: "black", sex: "female"},
-                {name: "Jack", species: "Lab", age: 1, color: "black", sex: "male"},
-                {name: "Koda", species: "Husky", age: 8, color: "Red and White", sex: "male"},
-                {name: "Bruce", species: "Mastiff", age: 3, color: "Brown", sex: "male"},
-                {name: "Lola", species: "French Bulldog", age: 2, color: "Grey", sex: "female"}
-            ];
+        appGrid: Grid
+    },
+    methods: {
+        foo: function() {
+            return this.$http.get('http://localhost:4040/v1/mammals')
+            .then(res => {
+                this.mammals=res.data;
+            });
         }
-
+    },
+    mounted: function() {
+        this.foo();
+    }
 }
 </script>
 
---
+++ src/components/grid/Grid.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <ag-grid-vue style="width: 5000px; height: 250px;"
+        class="ag-theme-material"
+        :gridOptions="gridOptions"
+        :columnDefs="columnDefs"
+        :rowData="rowData">
+    </ag-grid-vue>
+    
+</template>
+
+<script>
+import {AgGridVue} from "ag-grid-vue";
+export default {
+    props: ['rows'],
+    data()  {
+        return {
+            gridOptions: {},
+            columnDefs: null,
+            rowData: null
+        }
+    },
+    components: {
+        'ag-grid-vue': AgGridVue
+        },
+    created: function(){
+        console.log('created');
+},
+    beforeMount: function() {
+
+        console.log(this.rows)
+            this.columnDefs = [
+                {headerName: 'Age', field: 'age'},
+                {headerName: 'Breed', field: 'breed'},
+                {headerName: 'Coloring', field: 'coloring'},
+                {headerName: 'Date', field: 'date'},
+                {headerName: 'ID', field: 'id'},
+                {headerName: 'Name', field: 'name'},
+                {headerName: 'Species', field: 'species'}
+            ];
+            this.rowData = this.rows
+        },
+}
+</script>
