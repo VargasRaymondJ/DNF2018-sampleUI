38cdfc68bb6ff7bf8071336127b7032e987031a0
Raymond Vargas <vargas_raymond@bah.com>
2018-06-02 15:40:15 -0400

    knights components

---
+++ src/components/Knights.vue
@@ -0,0 +1,47 @@
+<template>
+    <section id="services" class="bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2>Vegas Golden Knights</h2>
+            <hr>
+            <app-grid :rows="knights"></app-grid>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+import Grid from './grid/Grid';
+export default {
+    data()  {
+        return {
+            knights: []
+        }
+    },
+    components: {
+        appGrid: Grid
+    },
+    methods: {
+        foo: function() {
+            return this.$http.get('http://localhost:8081/knights')
+            .then(res => {
+                this.knights=res.data;
+            });
+        }
+    },
+    mounted: function() {
+        this.foo();
+    }
+}
+</script>
+
+<style>
+    section {
+        padding: 150px 0;
+    }
+    .grid {
+        width: 500px
+    }
+</style>
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import Caps from './components/Caps.vue';
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 import Home from './components/Home/Home';
+import Knights from './components/Knights';
 
 
 
 
 export const routes = [
     {path: '', components: {default: Home}},
-    { path: '/caps', component: Caps}
+    { path: '/caps', component: Caps},
+    { path: '/knights', component: Knights}
 ]
